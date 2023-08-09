086eeaff267a4f4e768f89ecf60ef748055a61e7
hanskim431 <fogy431@gmail.com>
2023-08-09 12:12:07 +0900

    STS: workspace copied

---
+++ workspace-sts/board_project/src/main/java/com/jafa/domain/reply/ReplyVO.java
@@ -0,0 +1,17 @@
+package com.jafa.domain.reply;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyVO {
+	private Long rno;
+	private String memberId;
+}
